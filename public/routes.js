3a8d4bde2a10004f16829b0877e541557cbc702c
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-07-24 19:18:04 -0400

    finished note creation

--- public/routes.js
+++ public/routes.js
@@ -1,3 +1,28 @@
 const router = require('express').Router();
-const path = require('path');
+const fs = require('fs').promises;
 const uuid = require('uuid');
+
+router.get('/notes', async (req, res) => {
+
+  const data = await fs.readFile('./db/db.json', 'utf8');
+  res.json(JSON.parse(data));
+})
+
+router.post('/notes', async (req, res) => {
+  const data = await fs.readFile('./db/db.json', 'utf8');
+  const notes = JSON.parse(data);
+
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuid.v4()
+  };
+
+  notes.push(newNote);
+  await fs.writeFile('./db/db.json', JSON.stringify(notes));
+
+  res.send(newNote);
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const bp = require('body-parser');
 const path = require('path');
 
 const app = express();
 
-// app.use('/api', require('./public/routes'));
+const routes = require('./public/routes');
+app.use(express.urlencoded({ extended: true }))
+app.use(bp.json());
+
+app.use(express.static('./public'))
+app.use('/api', routes);
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
 app.listen(3333);
